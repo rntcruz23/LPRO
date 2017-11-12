3a9a1961e2ec4f5495d7e952cd2aba8038cd952a
arturribeiro96 <arturjcribeiro@gmail.com>
2017-11-12 16:03:37 +0000

    Input segundo a notacao algebrica
    Cavalo -> agora e N
    Rei -> agora e K
    para utilizar notaçao algebrica em ingles

--- mainQuest/src/pieces/Knight.java
+++ mainQuest/src/pieces/Knight.java
@@ -2,7 +2,7 @@ package pieces;
 
 public class Knight extends Peca {
 	public Knight(int[] startPos,color piece) {
-		super(startPos,piece,'K');
+		super(startPos,piece,'N');
 		calcMoves = new int[][] {{1,2},{1,-2},{-1,2},{-1,-2},{2,1},{2,-1},{-2,1},{-2,-1}};
 	}
 }
