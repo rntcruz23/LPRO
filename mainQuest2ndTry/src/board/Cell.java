ef428f25a85256bc03b686558e8212e638622fff
arturribeiro96 <arturjcribeiro@gmail.com>
2017-11-13 12:20:53 +0000

    cell -> constructor

--- mainQuest2ndTry/src/board/Cell.java
+++ mainQuest2ndTry/src/board/Cell.java
@@ -1,5 +1,49 @@
 package board;
 
-public class Cell {
+import pieces.Piece;
+import pieces.Pawn;
+import pieces.Rook;
+import pieces.Knight;
+import pieces.Bishop;
+import pieces.Queen;
+import pieces.King;
 
+public class Cell {
+	private int[] position;
+	private Piece piece;
+	private boolean empty;
+	
+	public Cell(int posHorizontal, int posVertical, char pieceName, Piece.color pieceColor) {
+		position = new int[] {posHorizontal, posVertical};
+		switch(pieceName) {
+		case 'P': 
+			piece = new Pawn(pieceColor);
+			empty = false;
+			break;
+		case 'R': 
+			piece = new Rook(pieceColor);
+			empty = false;
+			break;
+		case 'N': 
+			piece = new Knight(pieceColor);
+			empty = false;
+			break;
+		case 'B': 
+			piece = new Bishop(pieceColor);
+			empty = false;
+			break;
+		case 'Q': 
+			piece = new Queen(pieceColor);
+			empty = false;
+			break;
+		case 'K': 
+			piece = new King(pieceColor);
+			empty = false;
+			break;
+		default:
+			empty = true;
+			break;
+		
+		}
+	}
 }
