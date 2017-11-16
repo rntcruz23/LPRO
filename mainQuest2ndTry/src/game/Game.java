93a54272347e1e00c3bc52566b837a29b99f6bc1
arturribeiro96 <arturjcribeiro@gmail.com>
2017-11-16 22:48:18 +0000

    fazer movimento do peao preto

--- mainQuest2ndTry/src/game/Game.java
+++ mainQuest2ndTry/src/game/Game.java
@@ -1,16 +1,66 @@
 package game;
 
+import java.util.Scanner;
+
 import board.Board;
 import pieces.Piece;
 
 public class Game {
+	static boolean flagChange = false;
 	public static void main(String[] args) {
 		Board board = new Board();
-		int[] test1 = new int[] {0,1};
-		int[] test2 = new int[] {0,2};
-		board.printBoard(Piece.color.white);
-		//board.printBoard(Piece.color.black);
-		board.move(test1, test2);
-		board.printBoard(Piece.color.white);
+		Scanner input = new Scanner(System.in);
+		String move;
+		int[][] in;
+		Piece.color side = Piece.color.white;
+		
+		while(true) {
+			flagChange = false;
+			board.printBoard(side);
+			move = input.nextLine();
+			in = inputInt(move);
+			retHandler(board.move(in[0], in[1]));
+			if(flagChange && side == Piece.color.white) side = Piece.color.black;
+			else if(flagChange && side == Piece.color.black) side = Piece.color.white;
+		}
+	}
+	public static int[][] inputInt(String string) {
+		if(string.length() != 4) return null;
+		
+		int[] init = new int[2];
+		int[] fin = new int[2];
+		init[0] = string.charAt(0) - 'a';
+		init[1] = string.charAt(1) - '1';
+		fin[0] = string.charAt(2) - 'a';
+		fin[1] = string.charAt(3) - '1';		
+		return new int[][] {init, fin};
+	}
+	public static void retHandler(int ret) {
+		switch(ret) {
+		case -2:
+			System.out.println("Posição final inválida!");
+			break;
+		case -1:
+			System.out.println("Posição inicial sem peça!");
+			break;
+		case 0:
+			System.out.println("A peça não pode fazer esse move.");
+			break;
+		case 1:
+			flagChange = true;
+			break;
+		case 2:
+			System.out.println("Não podes ir para cima de uma peça tua.");
+			break;
+		case 3:
+			System.out.println("Não podes saltar peças.");
+			break;
+		case 4:
+			flagChange = true;
+			break;
+		default:
+			break;
+			
+		}
 	}
 }
