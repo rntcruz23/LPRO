a6693fa077702de212968e79efdc75060a348ba0
arturribeiro96 <32261504+arturribeiro96@users.noreply.github.com>
2017-12-14 15:04:10 +0000

    Delete Knight.java

--- mainQuest/src/pieces/Knight.java
+++
@@ -1,8 +0,0 @@
-package pieces;
-
-public class Knight extends Peca {
-	public Knight(int[] startPos,color piece) {
-		super(startPos,piece,'N');
-		calcMoves = new int[][] {{1,2},{1,-2},{-1,2},{-1,-2},{2,1},{2,-1},{-2,1},{-2,-1}};
-	}
-}
