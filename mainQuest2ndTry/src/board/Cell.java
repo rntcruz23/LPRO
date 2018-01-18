476b981f81b0c79093cc676cf90f389149600a95
arturribeiro96 <32261504+arturribeiro96@users.noreply.github.com>
2018-01-18 16:07:04 +0000

    Create Cell.java

---
+++ mainQuest2ndTry/src/board/Cell.java
@@ -0,0 +1,107 @@
+package board;
+
+import pieces.Piece;
+import pieces.Pawn;
+import pieces.Rook;
+import pieces.Knight;
+import pieces.Bishop;
+import pieces.Queen;
+import pieces.King;
+
+/*
+ * Classe da célula/casa do tabuleiro de xadrez
+ */
+public class Cell {
+	private int[] position;
+	private Piece piece;
+	private boolean empty;
+	public static enum ccolor {white, black};
+	private ccolor cellColor;
+	
+	public Cell(int posHorizontal, int posVertical, char pieceName, Piece.color pieceColor) {
+		/*
+		 * Inicializa a célula com a sua posição, cor, e peça (se aplicável)
+		 * @param int posHorizontal: numero da coluna (column) (0 a 7)
+		 * @param int posVertical: numero da linha (row) (0 a 7)
+		 * @param char pieceName: nome da peça a colocar na casa. Se a casa for vazia, colocar ' ', por exemplo
+		 * @param Piece.color pieceColor: cor da peça a colocar na casa. Se a casa for vazia, o valor é irrelevante
+		 */
+		
+		position = new int[] {posHorizontal, posVertical};
+		switch(pieceName) {
+		case 'P': 
+			piece = new Pawn(pieceColor);
+			empty = false;
+			break;
+		case 'R': 
+			piece = new Rook(pieceColor);
+			empty = false;
+			break;
+		case 'N': 
+			piece = new Knight(pieceColor);
+			empty = false;
+			break;
+		case 'B': 
+			piece = new Bishop(pieceColor);
+			empty = false;
+			break;
+		case 'Q': 
+			piece = new Queen(pieceColor);
+			empty = false;
+			break;
+		case 'K': 
+			piece = new King(pieceColor);
+			empty = false;
+			break;
+		default:
+			empty = true;
+			break;
+		}
+		if(((position[0] + position[1]) % 2) == 0) {
+			cellColor = ccolor.black;
+		}
+		else {
+			cellColor = ccolor.white;
+		}
+	}
+	public boolean isEmpty() {
+		return empty;
+	}
+	public Cell.ccolor showColor() {
+		return cellColor;
+	}
+	public Piece getPiece() {
+		if(empty) {
+			return null;
+		}
+		else {
+			return piece;
+		}
+	}
+	public int[][] showPiecePossibleMoves() {
+		return piece.showPossibleMoves();
+	}
+	public char showPieceName() {
+		return piece.showName();
+	}
+	public int[] showPosition() {
+		return position;
+	}
+	public Piece.color showPieceColor() {
+		if(empty) {
+			return null;
+		}
+		else {
+			return piece.showColor();
+		}
+	}
+	public void moveOutPiece() {
+		piece.movePiece();
+		empty = true;
+		
+	}
+	public void moveInPiece(Piece piece) {
+		this.piece = piece;
+		empty = false;
+	}
+}
