723a6ed52319a99863bf1e6e445a5134174c99b6
arturribeiro96 <arturjcribeiro@gmail.com>
2018-01-21 10:36:11 +0000

    Lobby com lista de salas atualizada ao abrir

--- client/socketsClient/src/window/Window.java
+++ client/socketsClient/src/window/Window.java
@@ -2,12 +2,7 @@ package window;
 
 import java.awt.EventQueue;
 import java.awt.Font;
-import java.awt.Graphics2D;
-import java.awt.Image;
-import java.awt.RenderingHints;
-import java.awt.image.BufferedImage;
 
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 import user.User;
@@ -38,24 +33,6 @@ public class Window {
 	}
 	public User getUser() {
 		return u;
-	}
-	private ImageIcon getScaledImage(ImageIcon srcImgIcon, int w, int h){
-		Image srcImg = srcImgIcon.getImage();
-	    BufferedImage resizedImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
-	    Graphics2D g2 = resizedImg.createGraphics();
-
-	    g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-	    g2.drawImage(srcImg, 0, 0, w, h, null);
-	    g2.dispose();
-	    
-	    return new ImageIcon(resizedImg);
-	}
-	private ImageIcon createImageIcon(String path) {
-		java.net.URL imgUrl = getClass().getResource(path);
-		if(imgUrl != null)
-			return new ImageIcon(imgUrl);
-		System.out.println("No file path " + path);
-		return null;	
 	}
 	public JFrame getFrmChess() {
 		return frmChess;
